52821ebeb6abdb84624d7d7481b35f2f0509167e
Nick Haynes <36779922+nickhaynes@users.noreply.github.com>
2018-03-11 18:30:58 -0500

    Create smc.js

---
+++ src/scripts/smc.js
@@ -0,0 +1,16 @@
+function headerMenu() {
+    document.getElementById("menuOptions").classList.toggle("show");
+}
+
+window.onclick=function(event) {
+    if (!event.target.matches('.menuButton')) {
+        var dropdowns = document.getElementsByClassName("menuContent");
+        var i;
+        for (i=0; i<dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains ('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
