90dc839f9d04798876421f6f1dace584fbb0b31a
ZumDeWald <zumdewald@gmail.com>
2018-03-17 19:30:34 -0500

    created seperate branch and folder system for individual DEV pages. Started on mine

---
+++ src/developers/z/z.js
@@ -0,0 +1,32 @@
+/* Header and shrink/grow tagline from Nick  */
+
+function headerMenu() {
+    document.getElementById("menuOptions").classList.toggle("show");
+}
+
+window.onclick=function(event) {
+    if (!event.target.matches('.menuButton')) {
+        var dropdowns = document.getElementsByClassName("menuContent");
+        var i;
+        for (i=0; i<dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains ('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
+
+$(window).resize(function() {
+
+  if ($(this).width() < 800) {
+
+    $('.props').hide();
+
+  } else {
+
+    $('.props').show();
+
+    }
+
+});
