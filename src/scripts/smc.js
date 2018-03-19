b947d7e8863cf07d5acb91621640d55bbf337264
Nick Haynes <j.n.haynes2008@gmail.com>
2018-03-18 20:48:26 -0500

    Changed link tag href's, cleaned up CSS, added developer folder for Nick

--- src/scripts/smc.js
+++ src/scripts/smc.js
@@ -10,9 +10,7 @@ window.onclick=function(event) {
         var i;
         for (i=0; i<dropdowns.length; i++) {
             var openDropdown = dropdowns[i];
-            if (openDropdown.classList.contains ('show')) {
-                openDropdown.classList.remove('show');
-            }
+           
         }
     }
 }
