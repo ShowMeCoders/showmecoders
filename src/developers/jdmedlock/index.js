3c0afff9004e30e92c6b6357ad4fa7203904fa9c
jdmedlock <jdmedlock@gmail.com>
2018-04-04 11:30:54 -0500

    feature: Add basic card layout to page
    Add basic card layout to Jim's page
    
    Resolves: N/a
    See also: N/a

--- src/developers/jdmedlock/index.js
+++ src/developers/jdmedlock/index.js
@@ -1,19 +1,21 @@
 
-  const MIN_HEIGHT = 75; /* Minimum content box height */
+  const MIN_HEIGHT = 145; /* Minimum content box height */
 /**
  * @description Adjust the title wrapper height and center the title based on
  * the height of the content box and the pixel length of the title string.
  * @param {String} box The CSS ID of the content box
  */
 function adjustTitlePositions(boxId) {
-  contentHeight = $( boxId ).css( "height" ).slice(0,-2);
-  titleLth = $( boxId + " .content-title" ).width();
+  const contentHeight = $( boxId ).css( "height" ).slice(0,-2);
+  const titleLth = $( boxId + " .content-title" ).width();
+  // console.log(`${boxId} contentHeight:${contentHeight} titleLth:${titleLth}`);
+  // console.log(`calculated top: ${((contentHeight/2)+(titleLth/2))}`);
   if (contentHeight < MIN_HEIGHT) {
     $( boxId + " .content-title-wrapper" ).css( "height", MIN_HEIGHT+'px');
-    $( boxId + " .content-title" ).css( "top", ((MIN_HEIGHT/2)-10)+'px');
+    $( boxId + " .content-title" ).css( "top", ((MIN_HEIGHT/2)+(titleLth/2))+'px');
   } else {
     $( boxId + " .content-title-wrapper" ).css( "height", contentHeight+'px');
-    $( boxId + " .content-title" ).css( "top", ((MIN_HEIGHT/2)+(titleLth/2))+'px');
+    $( boxId + " .content-title" ).css( "top", ((contentHeight/2)+(titleLth/2))+'px');
   }
 }
 
