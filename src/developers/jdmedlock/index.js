df2818198eeb21b35e4fd44ff4802de13dd62596
jdmedlock <jdmedlock@gmail.com>
2018-04-05 17:37:03 -0500

    feature: Update title positioning
    Update title postitioning logic
    
    Resolves: N/a
    See also: N/a

--- src/developers/jdmedlock/index.js
+++ src/developers/jdmedlock/index.js
@@ -1,48 +1,38 @@
 
-const MIN_HEIGHT = 145; /* Minimum content box height */
-/**
- * @description Ajust the size of the embedded image to match that of its
- * container.
- * @param {String} boxID The CSS ID of the container box
- */
-function adjustImageSizes(boxId) {
-  const contentHeight = $( boxId ).css( "height" ).slice(0,-2) - 6;
-  $( boxId + " .pic" ).css( "width", contentHeight+'px');
-  $( boxId + " .pic" ).css( "height", contentHeight+'px');
-  $( boxId + " img" ).css( "max-width", contentHeight+'px');
-  $( boxId + " img" ).css( "max-height", contentHeight+'px');
-}
-
 /**
  * @description Adjust the title wrapper height and center the title based on
  * the height of the content box and the pixel length of the title string.
  * @param {String} boxID The CSS ID of the container box
  */
-function adjustTitlePositions(boxId) {
-  const contentHeight = $( boxId ).css( "height" ).slice(0,-2);
-  const titleLth = $( boxId + " .content-title" ).width();
+function adjustContainer(boxId) {
+  const contentHeight = parseInt($( boxId + " .content-area" ).css( "height" ).slice(0,-2));
+  const titleLth = parseInt($( boxId + " .title" ).width());
   console.log(`${boxId} contentHeight:${contentHeight} titleLth:${titleLth}`);
-  // console.log(`calculated top: ${((contentHeight/2)+(titleLth/2))}`);
-  if (contentHeight < MIN_HEIGHT) {
-    $( boxId + " .content-title-wrapper" ).css( "height", MIN_HEIGHT+'px');
-    $( boxId + " .content-title" ).css( "top", ((MIN_HEIGHT/2)+(titleLth/2))+'px');
-  } else {
-    $( boxId + " .content-title-wrapper" ).css( "height", contentHeight+'px');
-    $( boxId + " .content-title" ).css( "top", ((contentHeight/2)+(titleLth/2))+'px');
+  let newHeight = contentHeight < titleLth ? titleLth : contentHeight;
+  newHeight += 20;
+  console.log(`newHeight: ${newHeight}`);
+  $( boxId + " .content-title-wrapper" ).css( "height", newHeight+'px');
+  //$( boxId + " .content-title" ).css( "top", ((newHeight/2)+((newHeight-titleLth)))+'px');
+  $( boxId + " .content-title" ).css( "top", '68%');
+
+
+  const backgroundUrl = $( boxId + " .pic").attr("src");
+  if (backgroundUrl !== undefined) {
+    $( boxId + " .pic" ).css( "width", newHeight+'px');
+    $( boxId + " .pic" ).css( "height", newHeight+'px');
+    $( boxId + " .pic" ).css( "background", `url("${backgroundUrl}"`);
+    $( boxId + " .pic" ).css( "background-position", "center");
+    $( boxId + " .pic" ).css( "background-size", "cover");
   }
 }
 
 $(document).ready(function() {
-  adjustTitlePositions('#about-box');
-  adjustImageSizes('#about-box');
-  adjustTitlePositions('#family-box');
-  adjustImageSizes('#family-box');
-  adjustTitlePositions('#chingu-box');
-  adjustImageSizes('#chingu-box');
-  adjustTitlePositions('#career-box');
-  adjustImageSizes('#career-box');
-  adjustTitlePositions('#projects-box');
-  adjustTitlePositions('#pam-box');
-  adjustTitlePositions('#devgaido-box');
-  adjustTitlePositions('#ideanebulae-box');
+  adjustContainer('#about-box');
+  adjustContainer('#family-box');
+  adjustContainer('#chingu-box');
+  adjustContainer('#career-box');
+  adjustContainer('#projects-box');
+  adjustContainer('#pam-box');
+  adjustContainer('#devgaido-box');
+  adjustContainer('#ideanebulae-box');
 });
