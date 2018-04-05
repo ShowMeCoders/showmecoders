92f3175649017d146eeee66a608823ca6eb7ebeb
jdmedlock <jdmedlock@gmail.com>
2018-04-05 09:13:02 -0500

    feature: Added more images and adjusted image size
    Added more images and dynamically adjust sizes at runtime based on height of surrounding content box
    
    Resolves: N/a
    See also: N/a

--- src/developers/jdmedlock/index.js
+++ src/developers/jdmedlock/index.js
@@ -1,14 +1,27 @@
 
-  const MIN_HEIGHT = 145; /* Minimum content box height */
+const MIN_HEIGHT = 145; /* Minimum content box height */
+/**
+ * @description Ajust the size of the embedded image to match that of its
+ * container.
+ * @param {String} boxID The CSS ID of the container box
+ */
+function adjustImageSizes(boxId) {
+  const contentHeight = $( boxId ).css( "height" ).slice(0,-2) - 6;
+  $( boxId + " .pic" ).css( "width", contentHeight+'px');
+  $( boxId + " .pic" ).css( "height", contentHeight+'px');
+  $( boxId + " img" ).css( "max-width", contentHeight+'px');
+  $( boxId + " img" ).css( "max-height", contentHeight+'px');
+}
+
 /**
  * @description Adjust the title wrapper height and center the title based on
  * the height of the content box and the pixel length of the title string.
- * @param {String} box The CSS ID of the content box
+ * @param {String} boxID The CSS ID of the container box
  */
 function adjustTitlePositions(boxId) {
   const contentHeight = $( boxId ).css( "height" ).slice(0,-2);
   const titleLth = $( boxId + " .content-title" ).width();
-  // console.log(`${boxId} contentHeight:${contentHeight} titleLth:${titleLth}`);
+  console.log(`${boxId} contentHeight:${contentHeight} titleLth:${titleLth}`);
   // console.log(`calculated top: ${((contentHeight/2)+(titleLth/2))}`);
   if (contentHeight < MIN_HEIGHT) {
     $( boxId + " .content-title-wrapper" ).css( "height", MIN_HEIGHT+'px');
@@ -20,13 +33,16 @@ function adjustTitlePositions(boxId) {
 }
 
 $(document).ready(function() {
-
   adjustTitlePositions('#about-box');
+  adjustImageSizes('#about-box');
   adjustTitlePositions('#family-box');
+  adjustImageSizes('#family-box');
   adjustTitlePositions('#chingu-box');
+  adjustImageSizes('#chingu-box');
   adjustTitlePositions('#career-box');
+  adjustImageSizes('#career-box');
+  adjustTitlePositions('#projects-box');
   adjustTitlePositions('#pam-box');
   adjustTitlePositions('#devgaido-box');
   adjustTitlePositions('#ideanebulae-box');
-
 });
