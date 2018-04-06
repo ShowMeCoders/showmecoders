bc31a554bfe9a83276a6937a76ff746dc091a923
jdmedlock <jdmedlock@gmail.com>
2018-04-06 17:22:19 -0500

    feature: Add content and images
    Add additional content and images
    
    Resolves: N/a
    See also: N/a

--- src/developers/jdmedlock/index.js
+++ src/developers/jdmedlock/index.js
@@ -7,10 +7,8 @@
 function adjustContainer(boxId) {
   const contentHeight = parseInt($( boxId + " .content-area" ).css( "height" ).slice(0,-2));
   const titleLth = parseInt($( boxId + " .title" ).width());
-  console.log(`${boxId} contentHeight:${contentHeight} titleLth:${titleLth}`);
   let newHeight = contentHeight < titleLth ? titleLth : contentHeight;
   newHeight += 40;
-  console.log(`newHeight: ${newHeight}`);
   $( boxId + " .content-title-wrapper" ).css( "height", newHeight+'px');
 
   const backgroundUrl = $( boxId + " .pic").attr("src");
@@ -24,13 +22,7 @@ function adjustContainer(boxId) {
 }
 
 $(document).ready(function() {
-  adjustContainer('#about-box');
-  adjustContainer('#family-box');
-  adjustContainer('#chingu-box');
-  adjustContainer('#career-box');
-  adjustContainer('#pm-box');
-  adjustContainer('#projects-box');
-  adjustContainer('#pam-box');
-  adjustContainer('#devgaido-box');
-  adjustContainer('#ideanebulae-box');
+  $( "div .content-box" ).each(function( index, element ) {
+    adjustContainer(`#${$( element ).attr( 'id' )}`);
+  });
 });
